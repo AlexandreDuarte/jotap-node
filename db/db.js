1135f9435f17b34450f8f6cc8d2a9ea8e39679d7
AlexandreDuarte <alexandre.duarte.2804@gmail.com>
2021-11-27 14:14:39 +0000

    postgres implementation cleanup

--- db/db.js
+++ db/db.js
@@ -1,5 +1,10 @@
 const { Pool } = require('pg');
 
-const pool = new Pool();
+const pool = new Pool({
+    connectionString: process.env.HEROKU_POSTGRESQL_RED_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
 
 module.exports = pool;
