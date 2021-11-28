fe8e52fa1e13595ef9c0213452b3eb937c030e48
AlexandreDuarte <alexandre.duarte.2804@gmail.com>
2021-11-28 20:30:47 +0000

    use env variables

--- db/db.js
+++ db/db.js
@@ -1,5 +1,9 @@
 const { Pool } = require('pg');
 
-const pool = new Pool();
+const pool = new Pool({
+    user: process.env.USER,
+    database: process.env.DATABASE,
+    password: process.env.USER_KEY
+});
 
 module.exports = pool;
