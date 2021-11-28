6ce35b5260988f207b956650ec6f0e493f867447
AlexandreDuarte <alexandre.duarte.2804@gmail.com>
2021-11-28 20:14:16 +0000

    port change

--- db/db.js
+++ db/db.js
@@ -1,10 +1,5 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-    connectionString: process.env.HEROKU_POSTGRESQL_RED_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
+const pool = new Pool();
 
 module.exports = pool;
