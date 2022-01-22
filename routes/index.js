6971bae12f6ebb2be4ffcca9a751c85766889f5e
AlexandreDuarte <alexandre.duarte.2804@gmail.com>
2022-01-22 15:27:01 +0000

    separated index in to files according to id query property

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ router.get('/exhibitions', function(req, res, next) {
 router.get('/portfolio', async function(req, res, next) {
     if (req.query.id) {
         const { rows } = await pool.query('SELECT * FROM obras WHERE id = $1', [req.query.id]);
-        if (rows) res.render('index', { obra: rows[0], url: (new URL(req.url, `https://${req.headers.host}`)).href });
+        if (rows) res.render('index-image-id', { obra: rows[0], url: (new URL(req.url, `https://${req.headers.host}`)).href });
         return;
     }
 
