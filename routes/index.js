694088254bff2d434c9e26d7cf5e149684175aa3
AlexandreDuarte <alexandre.duarte.2804@gmail.com>
2022-06-04 10:07:29 +0100

    new color scheme

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var pool = require('../db/db');
+var pool = require('../db/db_local');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
